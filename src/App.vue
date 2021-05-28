d67b3fbf1a3028cb75d96f3fa844d788f9492baa
Aiman Anizan <anizanaimann1@gmail.com>
2021-05-28 19:26:21 +0800

    Upload source

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container mx-auto font-poppins bg-purple">
+        <Header />
+        <div class="flex flex-col lg:flex-row lg-flex-1">
+            <Hero />
+            <InputBox />
+        </div>
+    </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Hero from "./components/Hero.vue";
+import InputBox from "./components/InputBox.vue";
+
+export default {
+    name: "App",
+    components: {
+        Header,
+        Hero,
+        InputBox,
+    },
+};
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
+
+<style></style>
